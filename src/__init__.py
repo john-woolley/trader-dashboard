314451dffd81183710d3c505d966726859a8f02b
John Woolley <woolley@berkeley.edu>
2024-02-10 05:38:38 -0500

    Fixed db import.

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,5 @@
 from .ingestion import CVIngestionPipeline, used_cols, macro_cols
-from .db import add_worker, get_worker, get_workers, drop_workers_table, drop_jobs_table, create_jobs_table, create_workers_table
+from .db import (
+    add_worker, drop_workers_table, drop_jobs_table, create_jobs_table,
+    create_workers_table
+)
